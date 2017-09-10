e5f86222c87fde1a7b8230ad461de438929d6684
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2017-09-10 12:48:09 +0000

    WIP

--- initMaster.sh
+++ initMaster.sh
@@ -5,8 +5,15 @@ sudo cp /etc/kubernetes/admin.conf $HOME/
 sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
 
-wget https://git.io/weave-kube -O weave-daemonset.yaml
-sed -i 's/weaveworks\/weave-kube/weaveworks\/weave-kube-arm/g' weave-daemonset.yaml
-sed -i 's/weaveworks\/weave-npc/weaveworks\/weave-npc-arm/g' weave-daemonset.yaml
-kubectl apply -f weave-daemonset.yaml
-rm weave-daemonset.yaml
+#wget https://git.io/weave-kube -O weave-daemonset.yaml
+#sed -i 's/weaveworks\/weave-kube/weaveworks\/weave-kube-arm/g' weave-daemonset.yaml
+#sed -i 's/weaveworks\/weave-npc/weaveworks\/weave-npc-arm/g' weave-daemonset.yaml
+#kubectl apply -f weave-daemonset.yaml
+#rm weave-daemonset.yaml
+
+#wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+#sed -i 's/amd64/arm/g' kube-flannel.yml
+#kubectl apply -f kube-flannel.yml
+#rm kube--flannel.yml
+
+kubectl apply -f weave.yaml
