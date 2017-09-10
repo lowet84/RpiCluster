072a9b0dff9c2938e41c7418f04c6dd5e84c1369
Fredrik Löwenhamn <lowet84@gmail.com>
2017-09-10 12:48:37 +0200

    Update initMaster.sh

--- initMaster.sh
+++ initMaster.sh
@@ -8,4 +8,5 @@ export KUBECONFIG=$HOME/admin.conf
 wget https://raw.githubusercontent.com/weaveworks/weave/master/prog/weave-kube/weave-daemonset.yaml
 sed -i 's/weaveworks\/weave-kube/weaveworks\/weave-kube-arm/g' weave-daemonset.yaml
 sed -i 's/weaveworks\/weave-npc/weaveworks\/weave-npc-arm/g' weave-daemonset.yaml
+kubectl apply -f weave-daemonset.yaml
 rm weave-daemonset.yaml
