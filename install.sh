f7e6e08846d5423e5c30667ed6d5779a2d40363a
Fredrik Löwenhamn <lowet84@gmail.com>
2017-09-10 12:10:42 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z $1 ]; then
+  echo "enter a name for the cluster node"
+  exit 1
+fi
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update
+apt-get install -y libfuse2 vim wget jq kubeadm
+
