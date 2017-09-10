be34bfe65c1d525dc645c177c0918c7cbcd682ab
Fredrik Löwenhamn <lowet84@gmail.com>
2017-09-10 13:04:21 +0200

    Update initMaster.sh

--- initMaster.sh
+++ initMaster.sh
@@ -5,7 +5,7 @@ sudo cp /etc/kubernetes/admin.conf $HOME/
 sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
 
-wget https://raw.githubusercontent.com/weaveworks/weave/master/prog/weave-kube/weave-daemonset.yaml
+wget https://git.io/weave-kube -O weave-daemonset.yaml
 sed -i 's/weaveworks\/weave-kube/weaveworks\/weave-kube-arm/g' weave-daemonset.yaml
 sed -i 's/weaveworks\/weave-npc/weaveworks\/weave-npc-arm/g' weave-daemonset.yaml
 kubectl apply -f weave-daemonset.yaml
